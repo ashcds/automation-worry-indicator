9a52c0373f02d5d86553c24c7881817f7be6163c
Aishwarya <aishwarya.cherian@hotmail.com>
2025-07-12 14:53:35 -0400

    update app

--- app.py
+++ app.py
@@ -4,161 +4,37 @@ import joblib as jl
 import pandas as pd
 import numpy as np
 
-# Side-bar navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Resume", "Projects", "Capstone Project Overview", "Automation Worry Predictor"])
-
-# -------------------------------------
-# Page 1: Biographical Hompage 
-# -------------------------------------
-# Display homepage content
-if page == "Home":
-    st.title("Welcome to My Portfolio") # Title
-    st.header("About Me") # Header
-    st.markdown("""
-                Hello! I'm Aishwarya (Ash), a Senior Data Analyst in the home appliances industry. I have experience in various domains, including finance, consulting, and manufacturing. I enjoy working with data to solve complex business problems and drive strategic decision-making.
-
-                **Academic Background:**
-                I have a bachelors in Mechanical Engineering and Finance from the University of Sydney and a M.S. in Mechanical Engineering from the University of Michigan, Ann Arbor.
-
-                **Career Aspirations:**
-                I aspire to transition into a Data Scientist role and work at the intersection of ML/AI and strategy, where I can leverage my technical and business skills to drive strategic initiatives.
-
-                **Interests and Hobbies:**
-                In my free time I enjoy playing badminton, latin dancing, reading and travelling to new places. I also love to explore new technologies and stay updated with the latest trends in data science and machine learning.
-
-                """)
-    
-
-# -------------------------------------
-# Page 2: Resume 
-# -------------------------------------
-
-if page == "Resume":
-    st.title("Aishwarya Cherian") # Title
-    st.image("my_pic.jpg", width=200) # Display profile picture
-    st.header("Education") # Header
-    st.markdown("""
-                **M.S. Data Science**  
-                Eastern University, 2024 - 2025
-
-                **M.S. Mechanical Engineering, 4.0**   
-                University of Michigan - Ann Arbor, 2021 - 2022
-
-                **B.S. Mechanical Engineering (Honors) / Commerce (Finance)**   
-                University of Sydney, 2014 - 2018 
-    """)
-
-    st.header("Work Experience")
-    st.markdown("""
-                **Whirlpool Corporation**   
-                *Senior Data Analyst, 2022 - Present*   
-                - Led a cross-functional team of developers to successfully deliver project data requirements and prepare data for ingestion into GCP BigQuery for the data strategy implementation   
-                - Analyzed data with Postgres SQL and developed dashboards in Looker and Tableau on product development project progression, resources, electronics asset volumes and cost to allow easy access to actionable insights for engineers and EES Leadership reducing their analytics effort by 30%   
-                - Supported analytics for electronics Zero-base Plan Complexity Reduction Workstream and overall compliance to reduce legacy complexity by ~10% per year   
-
-                **Commonwealth Bank of Australia**   
-                *Data Analyst, Data and Decision Science, 2019 - 2020*   
-                - Analyzed large datasets of over 10M records with SQL to understand key drivers of trends in retail banking portfolio   
-                - Built interactive Tableau dashboards for management to monitor essential portfolio performance measures   
-                - Delivered key insights on the ~$500B home loan portfolio to set tolerances for pertinent risk metrics as part of the Risk Appetite Statement annual refresh to reduce risk by 15 basis points.
-                """)
-
-    st.header("Technical Skills")
-    st.markdown("""
-                **Programming Languages:** Python, SQL, R
-
-                **Data Analysis Tools:** Pandas, NumPy, Scikit-learn, Matplotlib, Seaborn, TensorFlow, Keras
-
-                **Data Visualization:** Tableau, Looker Studio 
-
-                **Databases and Cloud:** MySQL, PostgreSQL, AWS Redshift, GCP Bigquery   
-
-                **Certifications:** AWS Certified Cloud Practioner, Lean Six Sigma Green Belt (Black Belt Trained)
-    """)
-
-
-# -------------------------------------
-# Page 3: Other Projects 
-# -------------------------------------
-
-if page == "Projects":
-    st.title("My Projects")
-    st.markdown("""
-                - [Diabetes Risk Predictor](https://github.com/ashcds/US-diabetes-health-indicators)   
-
-                **More to come!**
-                """)
-
-
-# -------------------------------------
-# Page 3: Automation Worry Predictor 
-# -------------------------------------
-
-if page == "Capstone Project Overview":
-    st.title("Capstone Project Overview: Automation Worry Predictor")
-    st.markdown("""
-                **Problem Statement:**   
-                As AI and automation technologies continue to advance at a rapid pace, concerns about their impact on jobs and livelihoods is growing. 
-                This project aims to predict the likelihood of individuals feeling worried about job automation so that organizations can take proactive measures to address these concerns, provide opportunities for upskilling and support their employees.
-                
-                The model is trained on data obtained from PEW Research Center's survey on technology and automation (American Trends Panel Wave 27). 
-                
-                **Approach:**  
-                Problem framed as a supervised binary classification task.   
-                - Target variable: `ROBJOB3b_W27` (1 = worried, 0 = not worried)
-
-                Feature selection was done using chi-square test and SHAP to understand which features were the most relevant for model performance. Based on this analysis the following features were selected:
-                - `CARS3B_W27`
-                - `ROBJOB4B_W27`
-                - `CARS7B_W27`
-                - `ROBJOB4A_W27`
-                - `CARS7A_W27`
-
-                RandomOverSampler was used to balance the dataset as ~70% of the data was of positive class (worried about automation). The model was trained using Logistic Regression, XGBoost and Random Forest classifiers. Cross validation was used to evaluate model performance and hyperparameter tuning was done using GridSearchCV. The best performing model was selected based on F1 score.
-
-
-                **Results:**
-                - Best performing model: XGBoost Classifier with F1 score of 0.72 on test set. The F1 score for the positive class (worried about automation) was 0.80. 
-                - Model was saved as a pickle file for deployment.
-                """)
-
-
-# -------------------------------------
-# Page 5: Automation Worry Predictor 
-# -------------------------------------
-if page == "Automation Worry Predictor":
-    st.title("Automation Worry Predictor") # Title
-    st.markdown("""
+st.title("Automation Worry Predictor") # Title
+st.markdown("""
                 This application predicts the likelihood of individuals feeling worried about job automation based on their responses to a survey. 
                 The model is trained on data obtained from PEW Research Center's survey on technology and automation (American Trends Panel Wave 27).
                 """)
 
-    # Load the pre-trained model
-    model = jl.load('best_xgb_model.pkl')
+# Load the pre-trained model
+model = jl.load('best_xgb_model.pkl')
 
-    # Input fields for user to enter data
-    st.header("Please answer the following questions:")
-    cars3b = st.selectbox(
+# Input fields for user to enter data
+st.header("Please answer the following questions:")
+cars3b = st.selectbox(
         "How worried are you, if at all, about the development of driverless vehicles?", 
         ["--Select an Option--", "Very Worried", "Somewhat Worried", "Not Too Worried", "Not at All Worried"])
 
-    cars7a = st.selectbox(
+cars7a = st.selectbox(
         "How safe would you feel sharing the road with a driverless passenger vehicle?",
         ["--Select an Option--", "Very Safe", "Somewhat Safe", "Not Too Safe", "Not at All Safe"])
 
-    cars7b = st.selectbox(
+cars7b = st.selectbox(
         "How safe would you feel sharing the road with a driverless freight truck?", 
         ["--Select an Option--", "Very Safe", "Somewhat Safe", "Not Too Safe", "Not at All Safe"])
 
-    robjob4b = st.selectbox("If robots and computers were able to perform most of the jobs currently being done by humans, would it be likely or unlikely that inequality between rich and poor would be much worse than it is today?", 
+robjob4b = st.selectbox("If robots and computers were able to perform most of the jobs currently being done by humans, would it be likely or unlikely that inequality between rich and poor would be much worse than it is today?", 
                             ["--Select an Option--", "Yes Likely", "No, Not Likely"])
 
-    robjob4a = st.selectbox("If robots and computers were able to perform most of the jobs currently being done by humans, would it be likely or unlikely that people would have a hard time finding things to do with their lives?", 
+robjob4a = st.selectbox("If robots and computers were able to perform most of the jobs currently being done by humans, would it be likely or unlikely that people would have a hard time finding things to do with their lives?", 
                             ["--Select an Option--", "Yes Likely", "No, Not Likely"])
 
     # Convert input data to appropriate format for prediction
-    input_data = pd.DataFrame({
+input_data = pd.DataFrame({
         'CARS3B_W27': [1 if cars3b == "Very Worried" else 2 if cars3b == "Somewhat Worried" else 3 if cars3b=="Not Too Worried"  else 4],
         'ROBJOB4B_W27': [1 if robjob4b == "Yes Likely" else 2],
         'CARS7B_W27': [1 if cars7b == "Very Safe" else 2 if cars7b == "Somewhat Safe" else 3 if cars7b=="Not Too Safe"  else 4],
@@ -167,7 +43,7 @@ if page == "Automation Worry Predictor":
     })
 
     # Diplay prediction button and prediction result
-    if st.button("Predict"):
+if st.button("Predict"):
         prediction = model.predict(input_data)
         prob_worried = model.predict_proba(input_data)[0, 1] * 100
         prob_not_worried = 100 - prob_worried
